c6c18395abd22d619523204c88bbb41fc2081657
GuanXin Wang <Alex0972112871@gmail.com>
2020-12-16 15:23:07 +0800

    basic CRUD using http verbs

--- app.js
+++ app.js
@@ -17,18 +17,110 @@ app.listen(PORT, function() {
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true})
 
-const articleSchema = {
+const articleSchema = ({
     title: String,
     content: String
-}
+});
 
 const Article = mongoose.model("article", articleSchema);
 
-app.get("/articles", function(req, res){
-    Article.find({}, function(err, foundArticles){
-        if (err){console.log(err);}
-        else {
-            res.send(foundArticles);
-        }
+app.route("/articles")
+    .get(function(req, res){
+        Article.find({}, function(err, foundArticles){
+            if (err){console.log(err);}
+            else {
+                res.send(foundArticles);
+            }
+        });
+    })
+    .post(function(req, res){
+        const title =  req.body.title;
+        const content = req.body.content;
+    
+        const newArticle = new Article({
+            title: title,
+            content: content
+        });
+    
+        newArticle.save(function(err){
+            if (err) {console.log(err);}
+            else {
+                res.send("Successfully adding an article.");
+            }
+        });
+    
+        console.log(title);
+        console.log(content);
+    
+    })
+    .delete(function(req, res){
+        Article.deleteMany({}, function(err){
+            if (err) {console.log(err);}
+            else {
+                res.send("Successfully delete all the articles.");
+            }
+        });
     });
-});
+
+app.route("/articles/:articleId")
+    .get(function(req, res){
+
+        Article.findOne({title: req.params.articleId}, function(err, foundArticle){
+            if (err) {
+                console.log(err);
+            }else if (!foundArticle){
+                res.send("No article found");
+            }else {
+                res.send(foundArticle);
+            }
+        });
+    })
+    .put(function(req, res){
+
+        Article.update(
+            {title: req.params.articleId},
+            {title: req.body.title, content: req.body.content},
+            {overwrite: true},
+            function(err){
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                } else {
+                    res.send("Successfully update the entire article");
+                }
+            }
+        )
+    })
+    .patch(function(req, res){
+        
+        Article.updateOne(
+            {title: req.params.articleId},
+            {$set: req.body},
+            {overwrite: true},
+            function(err){
+                if (err){
+                    console.log(err);
+                    res.send(err);
+                } else {
+                    res.send("Successfully update the article");
+                    // the body includes the field and the content
+                    // such as {content: "The modified text here"}
+                    console.log(req.body);
+                }
+            }
+        )
+    })
+    .delete(function(req, res) {
+
+        Article.deleteOne(
+            {title: req.params.articleId},
+            function(err){
+                if (err){
+                    console.log(err);
+                    res.send(err);
+                } else {
+                    res.send("Successfully delete the article");
+                }
+            }
+        );
+    });
